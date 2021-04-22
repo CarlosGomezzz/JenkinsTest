2933f60dadf89561d3d3a2972e6dcf00a23d6efe
CarlosGomezzz <78942350+CarlosGomezzz@users.noreply.github.com>
2021-04-21 22:57:29 -0400

    update 2

--- src/test/java/Carlos/FirstTest.java
+++ src/test/java/Carlos/FirstTest.java
@@ -7,23 +7,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class FirstTest {
 
 	@Test
 	public void openFacebook() {
-		WebDriver driver = new ChromeDriver();
-
-		driver.get("https://facebook.com");
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
 
 		boolean exist = driver.findElement(By.xpath("//button[contains(@name, 'login')]")).isDisplayed();
 
 		assertTrue(exist);
 
-//		driver.manage().window().maximize();
 
-//		String expectedTitle = "Google";
-//		String actualTitle = driver.getTitle();
-//
-//		assertEquals(expectedTitle, actualTitle);
 	}
 }
